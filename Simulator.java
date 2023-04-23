a4d8de36741f7abc1431cea7d5f9b4a1ef7607fd
Nathan Te <nathan_te28@gwmail.gwu.edu>
2023-04-23 11:40:26 -0400

    Fixed simulator

--- Simulator.java
+++ Simulator.java
@@ -7,31 +7,31 @@ public class Simulator {
 	
     public static void main(String[] args) {
 
-        final String USAGE = "java Simulator numMice numCats numZombieCats rounds [randSeed]";
+        final String USAGE = "java Simulator numTrash numCollector rounds [randSeed]";
 
         boolean DEBUG = false;
         
         //parse arguments
-        if(args.length < 4){
+        if(args.length < 3){
             System.out.println("ERROR: missing arguments");
             System.out.println(USAGE);
             System.exit(1);
         }
         int numTrash = Integer.parseInt(args[0]);
         int numCollector = Integer.parseInt(args[1]);
-        int rounds = Integer.parseInt(args[3]);
+        int rounds = Integer.parseInt(args[2]);
 
         Random rand;
-        if(args.length > 4)
-            rand = new Random(Integer.parseInt(args[4]));
+        if(args.length > 3)
+            rand = new Random(Integer.parseInt(args[3]));
         else
             rand = new Random(100);
 
-        if(args.length > 5 && args[5].equals("--DEBUG")){
+        if(args.length > 4 && args[4].equals("--DEBUG")){
             DEBUG=true;
         }
 
-        // Populate city with walls, bunnies, zombies, and mice
+        // Populate city with Collectos and trash
         City city= new City(rand, numTrash, numCollector);
         int count = 0;
 
@@ -45,12 +45,12 @@ public class Simulator {
 
             //TODO...
             //
-            // Every N rounds, add a mouse
+            // Every N rounds, add trash
             if(count % N == 0){
                 city.addTrash();
             }
             
-            //Every M rounds, add a Cat
+            //Every M rounds, add a collector
             if(count % M == 0){
                 city.addCollector();
             }
